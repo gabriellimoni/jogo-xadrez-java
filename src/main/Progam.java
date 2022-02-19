84e120f6b300b707d01c3febb73f07f1011f63bc
Gabriel Limoni <gabrielblimoni@gmail.com>
2022-02-19 11:18:32 -0300

    feat: add class postion

--- src/main/Progam.java
+++ src/main/Progam.java
@@ -1,10 +1,12 @@
 package main;
 
+import boardgame.Position;
+
 public class Progam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
