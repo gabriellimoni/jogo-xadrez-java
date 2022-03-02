1b98fb073455f19d08a405120452cdfa6baead12
Gabriel Limoni <gabrielblimoni@gmail.com>
2022-03-02 10:19:15 -0300

    feat: add captured pieces print and control

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,6 +1,8 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -13,10 +15,11 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> capturedPieces = new ArrayList();
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, capturedPieces);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -29,7 +32,10 @@ public class Program {
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);		
+				if (capturedPiece != null) {
+					capturedPieces.add(capturedPiece);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
